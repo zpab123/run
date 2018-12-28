9eccec81f644daa227bc3ba801bc750bb4f494c3
张鹏（金山） <287206570@qq.com>
2018-12-28 17:47:13 +0800

    增加1个 gate 测试

---
+++ gate/main.go
@@ -0,0 +1,18 @@
+// /////////////////////////////////////////////////////////////////////////////
+// gate 服务器
+
+package main
+
+import (
+	"github.com/zpab123/world" // world 库
+)
+
+// 主入口
+func main() {
+	// 创建 app
+	app := world.CreateApp()
+	app.SetName("gate")
+
+	// 运行 app
+	app.Run()
+}
