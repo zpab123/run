8d01ba199b1122ea9dc0f508a1e21c10cc16e972
zp_amd <287206570@qq.com>
2019-01-13 16:31:22 +0800

    优化代码

--- gate/handler.go
+++ gate/handler.go
@@ -2,10 +2,6 @@
 
 package main
 
-import (
-	"github.com/zpab123/world/ifs" // 全局接口
-)
-
 // 注册消息处理
 
 type Handle struct {
@@ -13,6 +9,6 @@ type Handle struct {
 }
 
 // 消息处理
-func (this *Handle) OnMessage(session ifs.ISession, data ifs.IMessage) {
+func (this *Handle) OnMessage(session interface{}, data interface{}) {
 
 }
