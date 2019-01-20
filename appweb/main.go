d64b8eef6b96fd6895d8c668678caa1f1839716a
张鹏（金山） <287206570@qq.com>
2019-01-20 11:59:38 +0800

    优化代码

--- appweb/main.go
+++ appweb/main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	// 设置日志输出
 	logs.Async(10000) // 异步日志缓冲 chan 的大小
-	//logs.SetLogger(logs.AdapterFile, `{"filename":"log.log,"level":2,"maxdays":10}`)
+	logs.SetLogger(logs.AdapterFile, `{"filename":"log.log","level":7,"maxdays":10}`)
 
 	beego.Run()
 }
