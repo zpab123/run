19672849592a9fc26f7d2f727b1f5a26167f7432
zp_amd <287206570@qq.com>
2019-02-01 17:37:40 +0800

    优化代码

--- gate/appdelegate.go
+++ gate/appdelegate.go
@@ -5,33 +5,40 @@ package main
 
 import (
 	"github.com/zpab123/world/session" // session
+	"github.com/zpab123/zaplog"        // log 库
 )
 
 // /////////////////////////////////////////////////////////////////////////////
 // AppDelegate 对象
 
 type AppDelegate struct {
+	clientPacketQueue chan *session.Message // 客户端消息队列
 }
 
 // 新建1个 AppDelegate
 func NewAppDelegate() *AppDelegate {
 	// 创建
-	ad := &AppDelegate{}
+	ad := &AppDelegate{
+		clientPacketQueue: make(chan *session.Message, 10000),
+	}
 
 	return ad
 }
 
-// session 消息管理
-func (this *AppDelegate) OnNewMessage(msg *session.Message) {
-	// 加入客户端消息队列
-}
-
 // 运行 AppDelegate
-func (this *AppDelegate) run() {
+func (this *AppDelegate) Run() {
 	for {
 		select {
-		//case item := <-clientPacketQueue:
-		//处理客户端消息
+		case item := <-this.clientPacketQueue:
+			//处理客户端消息
+			zaplog.Debugf(item.GetPacket().GetId())
+		default:
 		}
 	}
+}
+
+// session 消息管理
+func (this *AppDelegate) OnNewMessage(ses *session.FrontendSession, msg *session.Message) {
+	// 加入客户端消息队列
+	this.clientPacketQueue <- msg
 }
