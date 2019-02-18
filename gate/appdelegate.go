64d39f02ece5e6bad58b766cfdef0dfab19cc853
zp_amd <287206570@qq.com>
2019-02-18 21:51:11 +0800

    增加服务器消息

--- gate/appdelegate.go
+++ gate/appdelegate.go
@@ -13,6 +13,7 @@ import (
 
 type AppDelegate struct {
 	clientPacketQueue chan *session.Message // 客户端消息队列
+	serverPacketQueue chan *session.Message // 服务器消息队列
 }
 
 // 新建1个 AppDelegate
@@ -20,6 +21,7 @@ func NewAppDelegate() *AppDelegate {
 	// 创建
 	ad := &AppDelegate{
 		clientPacketQueue: make(chan *session.Message, 10000),
+		serverPacketQueue: make(chan *session.Message, 10000),
 	}
 
 	return ad
@@ -29,9 +31,12 @@ func NewAppDelegate() *AppDelegate {
 func (this *AppDelegate) Run() {
 	for {
 		select {
-		case item := <-this.clientPacketQueue:
-			//处理客户端消息
-			zaplog.Debugf(item.GetPacket().GetId())
+		case msg := <-this.clientPacketQueue:
+			// 处理客户端消息
+			zaplog.Debugf(msg.GetPacket().GetId())
+		case msg := <-this.serverPacketQueue:
+			//
+			zaplog.Debugf(msg.GetPacket().GetId())
 		default:
 		}
 	}
@@ -45,5 +50,15 @@ func (this *AppDelegate) OnClientMessage(ses *session.FrontendSession, msg *sess
 
 // 收到1个新的服务器消息
 func (this *AppDelegate) OnServerMessage(ses *session.BackendSession, msg *session.Message) {
+	this.serverPacketQueue <- msg
+}
+
+// 处理客户端消息
+func handleClientPacket() {
+
+}
+
+// 处理服务器消息
+func handleServerPacket() {
 
 }
