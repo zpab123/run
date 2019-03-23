306312874f732b54a91d8ae70ac3d25208f7d16d
zp_amd <287206570@qq.com>
2019-03-23 13:13:22 +0800

    增加协议处理

--- light/protocol/m_protocol.go
+++ light/protocol/m_protocol.go
@@ -1,15 +1,24 @@
 package protocol
 
+// 消息类型
+const (
+	C_TYPE_HANDSHAKE     uint8 = iota + 1 // 握手消息
+	C_TYPE_HANDSHAKE_ACK                  // 客户端->服务器握手ACK
+	C_TYPE_HEARTBEAT                      // 心跳消息
+	C_TYPE_DATA                           // 数据
+	C_TYPE_KICK                           // 服务器主动断开
+)
+
 // 主协议常量
 const (
-	C_MT_DATA  = iota // 主协议1
-	C_MT_DATA1        // 主协议2
-	C_MT_DATA2        // 主协议3
+	C_MT_DATA  uint16 = iota // 主协议1
+	C_MT_DATA1               // 主协议2
+	C_MT_DATA2               // 主协议3
 )
 
 // 自协议常量
 const (
-	C_SUB_DATA  = iota // 子协议1
-	C_SUB_DATA1        // 子协议2
-	C_SUB_DATA2        // 子协议3
+	C_SUB_DATA  uint16 = iota // 子协议1
+	C_SUB_DATA1               // 子协议2
+	C_SUB_DATA2               // 子协议3
 )
--- light/session.go
+++ light/session.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"run/light/protocol" // 消息协议
 
@@ -22,29 +23,52 @@ func (this *Session) Recv() {
 			break
 		}
 
-		if 0 == len(data) {
+		if len(data) < 3 {
 			break
 		}
 
-		go this.decode(data)
+		go this.handlePacket(data)
 	}
 
 	return
 }
 
-// 消息解码
-func (this *Session) decode(data []byte) {
-	fmt.Println(data)
+// packet 层处理
+func (this *Session) handlePacket(data []byte) {
+	t := uint8(data[0])                         // 类型
+	le := binary.LittleEndian.Uint16(data[1:3]) // 长度
 
-	str := string(data[:])
+	switch t {
+	case protocol.C_TYPE_HANDSHAKE: // 客户端->服务器握手请求
+	case protocol.C_TYPE_DATA: // 数据类
+		this.handleMsg(le, data[3:])
+	}
+}
 
-	fmt.Println(str)
+// message 层处理
+func (this *Session) handleMsg(length uint16, body []byte) {
+	if length < 4 {
+		return
+	}
+
+	mtype := binary.LittleEndian.Uint16(body[0:2])
+	stype := binary.LittleEndian.Uint16(body[2:4])
+
+	if length > 4 {
+		this.distribute(mtype, stype, body[4:])
+	} else {
+		this.distribute(mtype, stype, nil)
+	}
 }
 
 // 分发消息
-func (this *Session) distribute(mt int, sub int, data interface{}) {
-	switch mt {
-	case protocol.C_MT_DATA:
+func (this *Session) distribute(mtype uint16, stype uint16, data []byte) {
+	fmt.Println(mtype)
+	fmt.Println(stype)
+	fmt.Println(data)
+
+	switch mtype {
+	case protocol.C_MT_DATA1:
 		{
 
 		}
