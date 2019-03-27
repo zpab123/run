199f939654fdda8ce15bd2de8757021321eab9e3
张鹏（金山） <287206570@qq.com>
2019-03-27 10:37:36 +0800

    修改appDelegate模板

--- sco/11/appdelegate.go
+++ sco/11/appdelegate.go
@@ -4,7 +4,9 @@
 package main
 
 import (
-	"github.com/zpab123/sco/app" // 1个通用服务器库
+	"github.com/zpab123/sco/app"     // 1个通用服务器
+	"github.com/zpab123/sco/network" // 网络
+	"github.com/zpab123/sco/session" // 会话库
 )
 
 // /////////////////////////////////////////////////////////////////////////////
@@ -36,5 +38,15 @@ func (this *AppDelegate) OnRun(app *app.Application) {
 
 // app 停止运行
 func (this *AppDelegate) OnStop(app *app.Application) {
+
+}
+
+// 收到1个新的客户端消息
+func (this *AppDelegate) OnClientMessage(ses *session.ClientSession, packet *network.Packet) {
+
+}
+
+// 收到1个新的服务器消息
+func (this *AppDelegate) OnServerMessage(ses *session.ServerSession, packet *network.Packet) {
 
 }
