dde8fd98ee8b227e22b9a70e10f25eb8db6f078e
zp_amd <287206570@qq.com>
2019-04-03 07:47:06 +0800

    简化消息流程

--- sco/gate/appdelegate.go
+++ sco/gate/appdelegate.go
@@ -5,7 +5,6 @@ package main
 
 import (
 	"github.com/zpab123/sco/app"     // 1个通用服务器
-	"github.com/zpab123/sco/network" // 网络
 	"github.com/zpab123/sco/session" // 会话库
 )
 
@@ -27,22 +26,6 @@ func NewAppDelegate() *AppDelegate {
 func (this *AppDelegate) Init(app *app.Application) {
 }
 
-// app 开始运行
-func (this *AppDelegate) OnRun(app *app.Application) {
-
-}
-
-// app 停止运行
-func (this *AppDelegate) OnStop(app *app.Application) {
-
-}
-
 // 收到1个新的客户端消息
-func (this *AppDelegate) OnClientMessage(ses *session.ClientSession, packet *network.Packet) {
-
-}
-
-// 收到1个新的服务器消息
-func (this *AppDelegate) OnServerMessage(ses *session.ServerSession, packet *network.Packet) {
-
+func (this *AppDelegate) OnClentMsg(msg session.ClientMsg) {
 }
